01dc7bb17d09063cf193ff5e8fab00e7524cfe28
J.R. Hill <hiljusti@so.dang.cool>
2022-04-27 04:22:07 -0700

    Update alchemize.js

--- link-alchemizer/alchemize.js
+++ link-alchemizer/alchemize.js
@@ -1,7 +1,7 @@
 const main = () => {
   // Register the adder behavior
   document.querySelector('.add_row').onclick = addRow;
-  
+
   // Add a single row
   addRow();
 }
@@ -11,18 +11,19 @@ const addRow = () => {
   const tds = [0,0].map(() => document.createElement('td'));
   const textarea = document.createElement('textarea');
   const removeDiv = document.createElement('div');
-  
+
   textarea.placeholder = 'Bits of URLs separated by lines.';
-  
+
   removeDiv.className = 'remove_row';
   removeDiv.innerText = '-';
-  removeDiv.onlick = () => tr.remove();
-  
+
   tds[0].appendChild(textarea);
   tds[1].appendChild(removeDiv);
-  
+
   tds.forEach(td => tr.appendChild(td));
   getUrlChunksTable().appendChild(tr);
+
+  removeDiv.onlick = () => tr.remove();
 };
 
 const getUrlChunksTable = () => document.querySelector('#url_chunks');
