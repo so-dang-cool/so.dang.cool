d75370d3970c74626e00f0e86c679a6e12fe1efc
J.R. Hill <hiljusti@so.dang.cool>
2022-04-27 03:56:08 -0700

    Create alchemize.js

---
+++ link-alchemizer/alchemize.js
@@ -0,0 +1,21 @@
+const getUrlChunksTable = () => document.querySelector('#url_chunks');
+
+const addRow = () => {
+  const tr = document.createElement('tr');
+  const tds = [0,0].map(() => document.createElement('td'));
+  const textarea = document.createElement('textarea');
+  const removeDiv = document.createElement('div');
+  
+  textarea.placeholder = 'Bits of URLs separated by lines.';
+  
+  removeDiv.className = 'remove_row';
+  removeDiv.innerText = '-';
+  
+  tds[0].appendChild(textarea);
+  tds[1].appendChild(removeDiv);
+  
+  tds.forEach(td => tr.appendChild(td));
+  getUrlChunksTable().appendChild(tr);
+};
+
+addRow();
