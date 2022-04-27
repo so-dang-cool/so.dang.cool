4afc2730b92f8de8a176e269eb74037475bf2750
J.R. Hill <hiljusti@so.dang.cool>
2022-04-27 04:09:32 -0700

    Update alchemize.js

--- link-alchemizer/alchemize.js
+++ link-alchemizer/alchemize.js
@@ -1,5 +1,11 @@
 const getUrlChunksTable = () => document.querySelector('#url_chunks');
 
+const removeRow => clickedButton => {
+  const td = clickedButton.parentElement;
+  const tr = td.parentElement;
+  tr.remove()
+};
+
 const addRow = () => {
   const tr = document.createElement('tr');
   const tds = [0,0].map(() => document.createElement('td'));
@@ -10,6 +16,7 @@ const addRow = () => {
   
   removeDiv.className = 'remove_row';
   removeDiv.innerText = '-';
+  removeDiv.onlick = removeRow;
   
   tds[0].appendChild(textarea);
   tds[1].appendChild(removeDiv);
